ac155eb645c3490a51ff20d8bbc3a3ffef6fb5df
Daniel Bacher <danielbacher@phill93.de>
2019-06-05 22:45:49 +0200

    rename new fencer creation url

--- OFTM2/apps/fencers_management/urls.py
+++ OFTM2/apps/fencers_management/urls.py
@@ -7,5 +7,5 @@ app_name = 'fencers_management'
 urlpatterns = [
     path('', views.FencersListView.as_view(), name='fencers_list'),
     path('<int:fencer_id>', views.FencersDetailView.as_view(), name='fencer_detail'),
-    path('new', views.FencersCreateView.as_view(), name='fencer_create')
+    path('add', views.FencersCreateView.as_view(), name='fencer_create')
 ]
