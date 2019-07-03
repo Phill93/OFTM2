70d21660eff7eef32d599f6f40e50ade7248e670
Daniel Bacher <danielbacher@phill93.de>
2019-07-03 18:56:05 +0200

    add helper to calculate birthday

--- OFTM2/apps/fencers_management/helpers.py
+++ OFTM2/apps/fencers_management/helpers.py
@@ -6,3 +6,7 @@ def calculate_age(birthday: date):
     today = date.today()
     age = relativedelta(today, birthday).years
     return age
+
+def calculate_birthday(start: date, age: int):
+    birthday = start.today() - relativedelta(years=age)
+    return birthday
