f6d39da4ce3f54d99a16ef17e51e4cadbafb4577
Daniel Bacher <danielbacher@phill93.de>
2019-07-04 18:30:23 +0200

    add admin for combats

--- OFTM2/apps/tournament_management/admin.py
+++ OFTM2/apps/tournament_management/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from OFTM2.apps.tournament_management.models import Tournament
+
+from OFTM2.apps.tournament_management.models import Tournament, Combat
 
 
 @admin.register(Tournament)
 class TournamentAdmin(admin.ModelAdmin):
     list_display = ['name', 'ageclass', 'participants_count']
+
+
+@admin.register(Combat)
+class CombatAdmin(admin.ModelAdmin):
+    list_display = ['tournament', 'fighter1', 'fighter1_points', 'fighter2_points', 'fighter2']
