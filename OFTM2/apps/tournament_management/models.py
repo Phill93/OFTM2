7e758fa1998de5811c718d5d459245c8a56dc36e
Daniel Bacher <danielbacher@phill93.de>
2019-07-04 18:15:32 +0200

    add model for combats

--- OFTM2/apps/tournament_management/models.py
+++ OFTM2/apps/tournament_management/models.py
@@ -42,3 +42,51 @@ class Tournament(models.Model):
     class Meta:
         verbose_name = "Tunier"
         verbose_name_plural = "Turniere"
+
+
+class Combat(models.Model):
+    """Represents a combat"""
+    tournament = models.ForeignKey(
+        Tournament,
+        on_delete=models.CASCADE,
+        verbose_name="Tunier"
+    )
+
+    fighter1 = models.ForeignKey(
+        Fencer,
+        on_delete=models.CASCADE,
+        verbose_name="Kämpfer 1",
+        related_name="+"
+    )
+
+    fighter2 = models.ForeignKey(
+        Fencer,
+        on_delete=models.CASCADE,
+        verbose_name="Kämpfer 2",
+        related_name="+"
+    )
+
+    fighter1_points = models.IntegerField(
+        verbose_name="Punkte Fechter 1"
+    )
+
+    fighter2_points = models.IntegerField(
+        verbose_name="Punkte Fechter 2"
+    )
+
+    def __str__(self):
+        """Returns a assembeld name"""
+        return "Kampf: " + self.fighter1.__str__() + " vs " + self.fighter2.__str__() + " (Tunier " + self.tournament.__str__() + ")"
+
+    def get_winner(self):
+        """Returns the winner"""
+        if self.fighter1_points > self.fighter2_points:
+            return self.fighter1
+        elif self.fighter1_points == self.fighter2_points:
+            return None
+        else:
+            return self.fighter2
+
+    class Meta:
+        verbose_name = "Kampf"
+        verbose_name_plural = "Kämpfe"
